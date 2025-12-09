0b740e0f001703086cffb6f015e9aae3ce0ceada
George Chen <72078254+jiajic@users.noreply.github.com>
2025-12-08 20:57:23 -0500

    reverse compat: %||%

--- R/utils.R
+++ R/utils.R
@@ -97,3 +97,11 @@
     stop(sprintf("{%s} is not installed yet.", x), call. = FALSE)
     invisible()
 }
+
+# backwards compat
+if (getRversion() < "4.4.0") {
+    `%||%` <- function(x, y) {
+        if (is.null(x)) y else x
+    }
+}
+
